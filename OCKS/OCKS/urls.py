dd6a367b988e36dfc75db30f90c8fdab3673a5c6
Kenneth Vincent B. Castillo <202380191@psu.palawan.edu.ph>
2026-04-24 16:26:34 +0800

    bootstrap template setup

--- OCKS/OCKS/urls.py
+++ OCKS/OCKS/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from kioskapp.views import HomePageView
+from kioskapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.HomePageView.as_view(), name='home'),
+
 ]
--- OCKS/kioskapp/views.py
+++ OCKS/kioskapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from kioskapp.models import MenuItem, Order
 
 # Create your views here.
+class HomePageView(ListView):
+    model = MenuItem
+    context_object_name = 'home'
+    template_name = 'home.html'
+    
